029942e80f128d3e3d6ea4ed8e68fe8cfce87f1f
Shaik  Maviya <141839571+Maviyashek@users.noreply.github.com>
2023-10-27 13:20:35 +0530

    main file

---
+++ .py
@@ -0,0 +1,38 @@
+import instaloader
+import os
+
+def download_images(username, download_folder):
+    L = instaloader.Instaloader()
+
+    try:
+        # Load profile by username
+        profile = instaloader.Profile.from_username(L.context, username)
+
+        # Create the download folder if it doesn't exist
+        os.makedirs(download_folder, exist_ok=True)
+
+        # Download each image
+        for post in profile.get_posts():
+            if post.is_video or post.typename == 'GraphSidecar':  # Skip videos and albums
+                continue
+
+            image_url = post.url
+            image_filename = os.path.join(download_folder, f"{post.date_utc.strftime('%Y-%m-%d_%H-%M-%S')}.jpg")
+
+            # Check if the file already exists or if it has an unwanted extension
+            unwanted_extensions = ['.json.xyz', '.txt']
+            if not os.path.exists(image_filename) and not any(image_filename.lower().endswith(ext) for ext in unwanted_extensions):
+                # Download image
+                L.download_post(post, target=username)
+                print(f"Image downloaded: {image_filename}")
+
+    except instaloader.exceptions.InstaloaderException as e:
+        print(f"Error: {e}")
+
+
+
+if __name__ == "__main__":
+    insta_user_names = ['juli.annee','nikkisandiego','mariarzolaa','cathrynli', 'soyneiva', 'neivamara','missbuscemi','brazzersofficial','bangbros','vismaramartina']
+    download_folder = "downloaded_images"
+    for username in insta_user_names:
+        download_images(username, download_folder)
